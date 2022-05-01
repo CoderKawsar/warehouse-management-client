c8f5347ca2a1b3a82ee3e447b081f1c34036a011
Kawsar Ahmed <coder.kawsar@gmail.com>
2022-05-01 15:32:00 +0600

    skeliton components created

---
+++ src/components/InventoryItem/InventoryItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./InventoryItem.css";
+
+const InventoryItem = () => {
+  return (
+    <div>
+      <h3>Inventory Item</h3>
+    </div>
+  );
+};
+
+export default InventoryItem;
---
+++ src/components/ReputedClients/ReputedClients.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ReputedClients.css";
+
+const ReputedClients = () => {
+  return (
+    <div>
+      <h2>Reputed Clients</h2>
+    </div>
+  );
+};
+
+export default ReputedClients;
